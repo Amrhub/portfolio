a969f57a81476dd9191dc832c37e1d6cd91d720f
Taher <ttaherhagui@gmail.com>
2021-10-05 18:20:56 +0100

    Feat: Add closing functionnality to the menu.

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,24 @@
-const hamburger = document.getElementById('hamburger-menu');
+const hamburger = document.querySelector('.fa-bars');
+const linksWrapper = document.getElementById('linksWrapper');
+const cancelBtn = document.getElementById("cancel-btn");
+const links = Array.from(document.querySelectorAll("#linksWrapper ul li a"));
 
 function displayMenu() {
-  const linksWrapper = document.getElementById('linksWrapper');
-
+  hamburger.style.display = "none";
   linksWrapper.style.display = 'block';
   document.body.style.overflow = 'hidden';
+  
 }
 
+function closeMenu(){
+  linksWrapper.style.display = "none";
+  document.body.style.overflow = 'auto';
+  hamburger.style.display = "block"; 
+}
+
+
 hamburger.addEventListener('click', displayMenu);
+cancelBtn.addEventListener("click",closeMenu);
+
+links.map(function clickLinkHandler(x){
+   x.addEventListener("click",closeMenu)});
