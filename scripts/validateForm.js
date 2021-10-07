a2dc01a335d9f86bb2e8d004429c11bfe19ca5d4
Amrhub <amr_ahmed65@hotmail.com>
2021-10-08 01:45:03 +0200

    feat: add validation style

--- scripts/validateForm.js
+++ scripts/validateForm.js
@@ -10,18 +10,23 @@ form.addEventListener('submit', (e) => {
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const numRegex = /['1''2''0''3''4''5''6''7''8''9'\W]+/;
   const fullNameTest = fullName.value.split(' ');
+
+  form.elements[3].className = 'valid';
   // VALIDATION CHECK
   if (fullNameTest.length === 1) {
     fullNameError.textContent =
       'Enter your FULL name separated with whitespace';
+    fullName.className = 'invalid';
   } else {
     for (let i = 0; i < fullNameTest.length; i += 1) {
       if (numRegex.test(fullNameTest[i])) {
         fullNameError.textContent =
           'Name field must consists of alphabets and space only';
+        fullName.className = 'invalid';
         break;
       } else {
         fullNameError.textContent = '';
+        fullName.className = 'valid';
       }
     }
   }
@@ -29,8 +34,10 @@ form.addEventListener('submit', (e) => {
     emailError.textContent = 'Invalid Email address format ';
   } else if (email.value !== email.value.toLowerCase()) {
     emailError.textContent = 'Email address should be in lowercase';
+    email.className = 'invalid';
   } else {
     emailError.textContent = '';
+    email.className = 'valid';
   }
   // HERE
 
