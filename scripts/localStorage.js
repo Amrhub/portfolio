f810587d0363b487d1677c9fdf5f8c017ae38e4a
Amrhub <amr_ahmed65@hotmail.com>
2021-10-08 20:22:34 +0200

    feat: add localstorage caching

---
+++ scripts/localStorage.js
@@ -0,0 +1,34 @@
+(() => {
+  const form = document.getElementById('fs-frm');
+  const fullName = document.getElementById('full-name');
+  const email = document.getElementById('email-address');
+  const textArea = document.getElementById('message');
+  const stringifiedFormValues = localStorage.getItem('form');
+  const formValues = JSON.parse(stringifiedFormValues || '{}');
+  let formStored;
+
+  function storeValue(key, input) {
+    input.addEventListener('input', () => {
+      formValues[key] = input.value;
+      formStored = JSON.stringify(formValues);
+      localStorage.setItem('form', formStored);
+    });
+  }
+
+  window.onload = () => {
+    if (stringifiedFormValues) {
+      fullName.value = formValues.fullName;
+      email.value = formValues.email;
+      textArea.value = formValues.textArea;
+    }
+
+    storeValue('fullName', fullName);
+    storeValue('email', email);
+    storeValue('textArea', textArea);
+  };
+
+  form.onsubmit = () => {
+    localStorage.removeItem('form');
+    form.reset();
+  };
+})();
