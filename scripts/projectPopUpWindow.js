6752f7a412f0e7b1328d9de77fb938d6e5097ec5
Amr Ahmed <amr_ahmed65@hotmail.com>
2022-04-10 21:50:41 +0200

    Add Gitblogie

--- scripts/projectPopUpWindow.js
+++ scripts/projectPopUpWindow.js
@@ -7,8 +7,19 @@ const projects = [
       'https://user-images.githubusercontent.com/87689547/162473098-14c0c8fb-066c-4f94-86a7-f18f21b97958.png',
     featuredImageAlt: 'Project image',
     technology: ['Ruby on Rails', 'React', 'Tailwind'],
-    demoLive: 'https://amrhub.github.io/Module1-Capstone-Web-development-course/',
-    sourceLink: 'https://github.com/Amrhub/Module1-Capstone-Web-development-course',
+    demoLive: 'https://your-medbook.herokuapp.com/',
+    sourceLink: 'https://github.com/codepantha/medbook',
+  },
+  {
+    name: 'GitBlogie',
+    description:
+      'This application allows you to post your blog like and comments other blogs sign up and sign in to have more previlages',
+    featuredImageSource:
+      'https://user-images.githubusercontent.com/87689547/162636896-028ef2d8-532d-4629-840e-74eb9433c415.png',
+    featuredImageAlt: 'Project image',
+    technology: ['Ruby on Rails', 'Bootstrap_forms', 'Bootstrap'],
+    demoLive: 'https://gitblogie.herokuapp.com/',
+    sourceLink: 'https://github.com/Amrhub/blog-app/blob/dev/README.md',
   },
   {
     name: 'Microverse Program',
@@ -39,16 +50,6 @@ const projects = [
     technology: ['css', 'html', 'JavaScript'],
     demoLive: 'https://amrhub.github.io/To-Do-list/dist/',
     sourceLink: 'https://github.com/Amrhub/To-Do-list/',
-  },
-  {
-    name: 'Multi-Post Stories',
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
-    featuredImageSource: 'images/ImgPlaceholder.png',
-    featuredImageAlt: 'Project image',
-    technology: ['css', 'html', 'bootstrap', 'Ruby'],
-    demoLive: '',
-    sourceLink: 'https://github.com/Amrhub/myPortfolio',
   },
 ];
 
